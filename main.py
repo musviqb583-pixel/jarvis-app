1a14700279836dab652b783875083017795dea8b
musviqb583-pixel <musviqb583@gmail.com>
2026-08-26 14:33:38 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from kivy.app import App
+from kivy.uix.label import Label
+
+class JarvisApp(App):
+    def build(self):
+        return Label(text='Salam, Jarvis!')
+
+if __name__ == '__main__':
+    JarvisApp().run()
